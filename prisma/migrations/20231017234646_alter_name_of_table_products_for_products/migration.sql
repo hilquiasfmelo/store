808684ff523cea1ee8595b9cf9055b355d7ddcec
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-10-17 20:48:27 -0300

    chore: add init migration on supabase

---
+++ prisma/migrations/20231017234646_alter_name_of_table_products_for_products/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_categoryId_fkey";
+
+-- DropTable
+DROP TABLE "Product";
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "discountPercentage" INTEGER NOT NULL DEFAULT 0,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen w-full bg-gray-900  text-gray-200">
       <h1>Hello World</h1>
     </div>
   )
