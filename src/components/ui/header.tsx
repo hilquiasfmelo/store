8f02b543d9f88fa877fd98e21e277c747433048e
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-10-18 16:19:17 -0300

    feat: add auth handler header components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,19 +4,30 @@ import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   PercentIcon,
   ShoppingCartIcon,
 } from 'lucide-react'
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
 import { Card } from './card'
 import { Button } from './button'
+import { Avatar, AvatarFallback, AvatarImage } from './avatar'
+import { Separator } from './separator'
 
 export function Header() {
+  const { data, status } = useSession()
+
+  const isAuthenticated = status === 'authenticated'
+
   async function handleLoginGoogle() {
     await signIn()
+  }
+
+  async function handleLogoutGoogle() {
+    await signOut()
   }
 
   return (
@@ -33,15 +44,49 @@ export function Header() {
             Menu
           </SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-2">
-            <Button
-              onClick={handleLoginGoogle}
-              variant="outline"
-              className="w-full justify-start gap-2"
-            >
-              <LogInIcon size={16} />
-              Fazer login
-            </Button>
+          {isAuthenticated && data?.user && (
+            <div className="flex-flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+
+                <div className="flex flex-col">
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">
+                    👋 Olá! <p className="inline text-xs">Seja Bem-Vindo(a)</p>
+                  </p>
+                </div>
+              </div>
+
+              <Separator />
+            </div>
+          )}
+
+          <div className="mt-4 flex flex-col gap-2">
+            {isAuthenticated ? (
+              <Button
+                onClick={handleLogoutGoogle}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogOutIcon size={16} />
+                Fazer logout
+              </Button>
+            ) : (
+              <Button
+                onClick={handleLoginGoogle}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogInIcon size={16} />
+                Fazer login
+              </Button>
+            )}
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} />
